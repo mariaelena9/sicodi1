03ab9c4d2d9cb76bda98ef1a275a61753ee73920
Ricardo Villa <rialvillaos@ittepic.edu.mx>
2022-03-21 22:43:52 -0700

    [Fix] Solution of Sidebar. JJPL

--- app/src/Components/Categories/Categories.jsx
+++ app/src/Components/Categories/Categories.jsx
@@ -1,60 +1,61 @@
-import React from "react";
-import "./Categories.css";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
+import React from 'react';
+import './Categories.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export default function Categories({ setSelectedButton, selectedMode }) {
-  let buttons;
-  function clickOn(val) {
-    setSelectedButton(val);
-  }
-  if (selectedMode === "Principal" || selectedMode === "Recibidos") {
-    buttons = (
-      <ButtonGroup
-        onClick={(evt) => {
-          clickOn(evt.target.value);
-        }}
-        aria-label="Principal"
-      >
-        <Button variant="info" value="">
-          Todos
-        </Button>
-        <Button variant="success" value="Importante">
-          Importantes
-        </Button>
-        <Button variant="primary" value="Circular">
-          Circulares
-        </Button>
-        <Button variant="warning" value="Paquetería">
-          Paquetería
-        </Button>
-        <Button variant="danger" value="Oficio">
-          Oficios
-        </Button>
-      </ButtonGroup>
-    );
-  } else if (selectedMode === "Enviados") {
-    buttons = (
-      <ButtonGroup
-        onClick={(evt) => {
-          clickOn(evt.target.value);
-        }}
-        aria-label="Enviado"
-      >
-        <Button className="bEnviado" variant="primary" value="">
-          Todos
-        </Button>
-        <Button className="bEnviado" variant="danger" value="En Proceso">
-          En Proceso
-        </Button>
-        <Button className="bEnviado" variant="warning" value="Enviado">
-          Enviados
-        </Button>
-      </ButtonGroup>
-    );
-  } else {
-    clickOn("");
-    buttons = null;
-  }
-  return buttons;
+	let buttons;
+	function clickOn(val) {
+		setSelectedButton(val);
+	}
+	if (selectedMode === 'Principal' || selectedMode === 'Recibidos') {
+		buttons = (
+			<ButtonGroup
+				onClick={(evt) => {
+					clickOn(evt.target.value);
+				}}
+				aria-label='Principal'
+				style={{ zIndex: '-1' }}
+			>
+				<Button variant='info' value=''>
+					Todos
+				</Button>
+				<Button variant='success' value='Importante'>
+					Importantes
+				</Button>
+				<Button variant='primary' value='Circular'>
+					Circulares
+				</Button>
+				<Button variant='warning' value='Paquetería'>
+					Paquetería
+				</Button>
+				<Button variant='danger' value='Oficio'>
+					Oficios
+				</Button>
+			</ButtonGroup>
+		);
+	} else if (selectedMode === 'Enviados') {
+		buttons = (
+			<ButtonGroup
+				onClick={(evt) => {
+					clickOn(evt.target.value);
+				}}
+				aria-label='Enviado'
+			>
+				<Button className='bEnviado' variant='primary' value=''>
+					Todos
+				</Button>
+				<Button className='bEnviado' variant='danger' value='En Proceso'>
+					En Proceso
+				</Button>
+				<Button className='bEnviado' variant='warning' value='Enviado'>
+					Enviados
+				</Button>
+			</ButtonGroup>
+		);
+	} else {
+		clickOn('');
+		buttons = null;
+	}
+	return buttons;
 }
--- app/src/Components/MailContainer/MailContainer.jsx
+++ app/src/Components/MailContainer/MailContainer.jsx
@@ -1,78 +1,80 @@
-import React from "react";
-import "./MailContainer.css";
-import JSONData from "../../assets/Data.json";
-import MailItem from "../MailItem/MailItem";
-import "../Header/Header.css";
-import SearchBar from "../SearchBar/SearchBar";
-import Categories from "../Categories/Categories";
-import { useState } from "react";
+import React from 'react';
+import './MailContainer.css';
+import JSONData from '../../assets/Data.json';
+import MailItem from '../MailItem/MailItem';
+import '../Header/Header.css';
+import SearchBar from '../SearchBar/SearchBar';
+import Categories from '../Categories/Categories';
+import { useState } from 'react';
 
 export default function MailContainer({ selectedMode }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedButton, setSelectedButton] = useState("");
-  const f3 = (val) => {
-    if (selectedButton === "") {
-      return val;
-    } else if (val.area.includes(selectedButton)) {
-      return val;
-    }
-  };
-  const f2 = (val) => {
-    if (searchTerm === "") {
-      return val;
-    } else if (val.author.toLowerCase().includes(searchTerm.toLowerCase())) {
-      return val;
-    }
-  };
-  const f1 = (val) => {
-    switch (selectedMode) {
-      case "Principal":
-      case "Recibidos":
-        if (
-          val.area === "Paquetería" ||
-          val.area === "Oficio" ||
-          val.area === "Importante" ||
-          val.area === "Circular"
-        ) {
-          return val;
-        }
-        break;
-      case "Historico":
-        if (val.area === "Archivado") {
-          return val;
-        }
-        break;
-      case "Enviados":
-        if (val.area === "Enviado" || val.area === "En Proceso") {
-          return val;
-        }
-        break;
-    }
-  };
-  const filters = [f1, f2, f3];
-  return (
-    <div id="main" className="container">
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <div className="fs-4">{selectedMode}</div>
-      <Categories
-        setSelectedButton={setSelectedButton}
-        selectedMode={selectedMode}
-      />
+	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedButton, setSelectedButton] = useState('');
+	const f3 = (val) => {
+		if (selectedButton === '') {
+			return val;
+		} else if (val.area.includes(selectedButton)) {
+			return val;
+		}
+	};
+	const f2 = (val) => {
+		if (searchTerm === '') {
+			return val;
+		} else if (val.author.toLowerCase().includes(searchTerm.toLowerCase())) {
+			return val;
+		}
+	};
+	const f1 = (val) => {
+		switch (selectedMode) {
+			case 'Principal':
+			case 'Recibidos':
+				if (
+					val.area === 'Paquetería' ||
+					val.area === 'Oficio' ||
+					val.area === 'Importante' ||
+					val.area === 'Circular'
+				) {
+					return val;
+				}
+				break;
+			case 'Historico':
+				if (val.area === 'Archivado') {
+					return val;
+				}
+				break;
+			case 'Enviados':
+				if (val.area === 'Enviado' || val.area === 'En Proceso') {
+					return val;
+				}
+				break;
+			default:
+				return val;
+		}
+	};
+	const filters = [f1, f2, f3];
+	return (
+		<div id='main' className='container'>
+			<SearchBar setSearchTerm={setSearchTerm} />
+			<div className='fs-4'>{selectedMode}</div>
+			<Categories
+				setSelectedButton={setSelectedButton}
+				selectedMode={selectedMode}
+			/>
 
-      <div id="container" className="mailBox">
-        {JSONData.filter((v) => filters.every((f) => f(v))).map((val, key) => {
-          return (
-            <MailItem
-              author={val.author}
-              subject={val.subject}
-              content={val.content}
-              area={val.area}
-              status={val.status}
-              key={key}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+			<div id='container' className='mailBox'>
+				{JSONData.filter((v) => filters.every((f) => f(v))).map((val, key) => {
+					return (
+						<MailItem
+							author={val.author}
+							subject={val.subject}
+							content={val.content}
+							area={val.area}
+							status={val.status}
+							key={key}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
